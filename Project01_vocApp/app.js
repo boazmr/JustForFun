77613b846076b83f19723bdca4671de00214029e
Boaz Maron <boazmr112@gmail.com>
2025-03-18 20:08:10 +0200

    everything

--- Project01_vocApp/app.js
+++ Project01_vocApp/app.js
@@ -1,1 +1,25 @@
-console.log("HELLO WORLD")
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/home.html')
+})
+
+app.get('/login', async (req, res) => {
+    res.sendFile( __dirname + '/login.html')
+})
+app.post('/login', (req, res) => {
+    const {username, password} = req.body
+    console.log(username)
+    res.redirect('/')
+})
+
+app.all('*', (req, res) => {
+    console.log("error page")
+    res.send("page not found")
+})
+
+
+app.listen(port)
+console.log("APP IS RUNNING")
